bc9cd5dbb3df579eb2130b527bd3fa2ee92681e5
Henshou <gerrardgrs@gmail.com>
2025-05-17 19:07:47 +0700

    Feat: greedy best first

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        Board board = IO.readInput("C:\\Users\\luthf\\Documents\\INSTITUT TEKNOLOGI BANDUNG\\SEMESTER 4\\Tucil3_13523064_13523084\\src\\tes.txt");
+        Board board = IO.readInput("C:\\Semester-4\\Stima\\Tucil\\Tucil3_13523064_13523084\\src\\tes.txt");
         assert board != null;
         board.updateBoard();
-        List<Board> s = board.generatePossibleBoards();
+        Solver solver = new Solver();
+        solver.GBFSSolver(board);
+        solver.printVisited();
+//        List<Board> s = board.generatePossibleBoards();
 //        for (Board b : s) {
 //            System.out.println("Possible Board:");
 //            b.printBoard();
@@ -16,17 +19,17 @@ public class Main {
 //            System.out.println("Parent State: " + b.getParentState());
 //            System.out.println("goal: " + b.getGoal().getX() + " " + b.getGoal().getY());
 //        }
-        Board firstBoard = s.getFirst();
-        firstBoard.updateBoard();
-        firstBoard.printBoard();
-        s = firstBoard.generatePossibleBoards();
-        for (Board b : s) {
-            System.out.println("Possible Board:");
-            b.printBoard();
-            System.out.println("Move: " + b.getLatestMove());
-            System.out.println("Parent State: " + b.getParentState());
-            System.out.println("goal: " + b.getGoal().getX() + " " + b.getGoal().getY());
-        }
+//        Board firstBoard = s.getFirst();
+//        firstBoard.updateBoard();
+//        firstBoard.printBoard();
+//        s = firstBoard.generatePossibleBoards();
+//        for (Board b : s) {
+//            System.out.println("Possible Board:");
+//            b.printBoard();
+//            System.out.println("Move: " + b.getLatestMove());
+//            System.out.println("Parent State: " + b.getParentState());
+//            System.out.println("goal: " + b.getGoal().getX() + " " + b.getGoal().getY());
+//        }
     }
 }
 
